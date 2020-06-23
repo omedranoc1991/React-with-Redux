19ff761115311da1dd476edba719385723906553
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-06-23 14:24:03 -0500

    I added the firts lessons in the course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// component
+const Test = ()=>{                     
+var text = "Click me"     
+    return (
+<div>
+  <babel className='label' htmlFor='test'>enter name</babel>
+  <input id='test' type='text' style={{border:'1px solid red'}}></input>
+  <button style={{backgroundColor: 'blue', color : 'white'}}>{text}</button>
+</div>
+    )
+    
+} 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//render components
+ReactDOM.render(<Test />, document.getElementById('root'))
+
+
